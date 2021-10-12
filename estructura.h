e52a2a25413f04949400f1fe874466b02a48bef3
AxelGarciaTello <gata21022000@hotmail.com>
2021-10-12 16:08:11 -0500

    Estructura para los datos

---
+++ estructura.h
@@ -0,0 +1,12 @@
+#ifndef ESTRUCTURA_H
+#define ESTRUCTURA_H
+
+typedef struct localizacion{
+	char tiempo[9];
+	char latitud[10];
+	char longitud[11];
+	char NS;
+	char EW;
+} LOCALIZACION;
+
+#endif
